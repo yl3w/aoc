8b73a37c2e359aacf85dc3cd0c3e784a794f4ef1
Bhaskar <maddalab@gmail.com>
2018-12-01 20:17:16 -0500

    Day 1 of advent of code

---
+++ 2018/day1/frequency.py
@@ -0,0 +1,26 @@
+fchanges = []
+with open('frequency_input') as f:
+	for line in f:
+		fchanges.append(int(line))
+
+current = 0
+frequencies = []
+for f in fchanges:
+	current += f
+	if current in frequencies:
+		print('found duplicate in first cycle ' + str(current))
+	frequencies.append(current)
+
+
+print('Resulting skew is ' + str(current))
+
+foundDuplicate = False
+while not foundDuplicate:
+	for f in fchanges:
+		current += f
+		if current in frequencies:
+			print('found first repetition ' + str(current))
+			foundDuplicate = True
+			break
+
+
