02f984a45dd0ad16af0e163eb2e6cf414d5fa0cb
Bhaskar <maddalab@gmail.com>
2018-12-05 10:03:53 -0500

    --- Day 5: Alchemical Reduction --- --- Part Two --- https://adventofcode.com/2018/day/5#part2

--- 2018/day5/polymer.py
+++ 2018/day5/polymer.py
@@ -25,18 +25,28 @@ def dissimilarUnits(ut):
 def verifyPolymer(polymer):
     p1, p2 = itertools.tee(polymer)
     next(p2, None)
-    return functools.reduce(lambda s, u: s if not s else dissimilarUnits(u), zip(p1, p2), True)
+    if not functools.reduce(lambda s, u: s if not s else dissimilarUnits(u), zip(p1, p2), True):
+        raise Exception('Found a bad polymer ' + polymer)
+
+def removeUnit(polymer, unit):
+    return polymer.replace(unit, '').replace(unit.upper(), '')
+
+def doReactionWithBadUnit(polymer, unit):
+    polymer = doReaction(removeUnit(polymer, unit))
+    #printPolymer(polymer)
+    return polymer
 
 def printPolymer(polymer):
     print('Resulting polymer is ' + polymer)
     print('Length of resulting polymer is ' + str(len(polymer)))
-    print('Verifying polymer, Is polymer good? ' + str(verifyPolymer(polymer)))
 
 def reactOnAllPolymers(filename):
     with open(filename) as f:
         for polymer in f:
-            printPolymer(doReaction(polymer.strip()))
-
+            print('Length of polymer originally presented after reactions ' + str(len(doReaction(polymer.strip()))))
+            length = min(map(lambda u: len(doReactionWithBadUnit(polymer.strip(), u)), 'abcdefghijklmnopqrstuvwxyz'))
+            print('Found shortest polymer with length ' + str(length) + ' after eliminating bad unit')
+ 
 
 polymer = "bbbBBBcC"
 
