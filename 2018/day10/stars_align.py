cbd2bb471725e6077ed8bbe9852a5a370d284c5d
Bhaskar <maddalab@gmail.com>
2018-12-14 07:11:52 -0500

    Completed day 10
    There are some very cool closed form solutions that others have implemented, mine uses a more
    naive implementation. The insight here is that the area of the enclosing points is minimized
    when all the points come together.

--- 2018/day10/stars_align.py
+++ 2018/day10/stars_align.py
@@ -2,6 +2,7 @@
 # https://adventofcode.com/2018/day/10
 
 import re
+from itertools import groupby, tee
 
 def readSpecification(filename):
     with open(filename) as f:
@@ -12,34 +13,55 @@ def readSpecification(filename):
 def splitPointsAndVelocity(pandv):
     return (list(map(lambda pv: (pv[0], pv[1]), pandv)), list(map(lambda pv: (pv[2], pv[3]), pandv)))
 
-def transition(points, velocity):
-    return list(map(lambda pv: (pv[0][0]+pv[1][0], pv[0][1]+pv[1][1]), zip(points, velocity)))
+def transition(points, velocity, duration):
+    return list(map(lambda pv: (pv[0][0]+pv[1][0]*duration, pv[0][1]+pv[1][1]*duration), zip(points, velocity)))
 
-def isValidText(points):
-    return all([any([(x-1,y) in points, (x+1,y) in points, (x,y-1) in points, (x, y+1) in points]) for x, y in points])
-
-def printPoints(points):
+def area(points):
+    minx = min(map(lambda p: p[0], points))
+    maxx = max(map(lambda p: p[0], points))
+    
     miny = min(map(lambda p: p[1], points))
     maxy = max(map(lambda p: p[1], points))
 
-    for r in range(miny, maxy + 1):
-        positions = list(map(lambda p: p[0], sorted(filter(lambda p: p[1] == r, points), key=lambda p: p[0])))
-        if len(positions) > 0:
-            row = [' '] * (max(positions) + 1)
-            for position in positions:
-                row[position] = '#'
-            print("".join(row))
+    return (maxx - minx) * (maxy - miny)
 
-fname='sample.input'
-#fname='production.input'
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+def printPoints(points):
+    points.sort(key=lambda p: p[1])
+    groupedByY = {k:list(v) for k, v in groupby(points, key=lambda p: p[1])}
+    miny = min(map(lambda p: p[1], points))
+    maxy = max(map(lambda p: p[1], points))
+    minx = min(map(lambda p: p[0], points))
+
+    for y in range(miny, maxy + 1):
+        xs = list(sorted(set(map(lambda p: p[0], groupedByY[y]))))
+        xs.insert(0, minx)
+        for s, e in pairwise(xs):
+            if s != e:
+                print(' ' * (e - s - 1), end='')
+            print('#', end='')
+        print('')
+
+#fname='sample.input'
+fname='production.input'
 points, velocity = splitPointsAndVelocity(readSpecification(fname))
+duration = 0
+currentArea = area(points)
+minArea = currentArea
+while minArea >= currentArea:
+    duration += 1
+    minArea = currentArea
+    currentArea = area(transition(points, velocity, duration))
 
-index = 0
-while index < 5 or isValidText(points):
-    points = transition(points, velocity)
-    index += 1
+print('Found time, it is ' + str(duration - 1))
+result = transition(points, velocity, duration - 1)
+print('Result Area ' + str(area(result)))
+printPoints(result)
 
-
-printPoints(points)
 
 
