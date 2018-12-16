cbee5813061940a8bb5087407e9146aa4f80a00d
Bhaskar <maddalab@gmail.com>
2018-12-16 14:50:01 -0500

    Complete part 2. I surprised myself by doing this very quickly and without any error :-)
    There might be a minor optimization that can be done with array slicing
    but code completes in a couple of minutes and I like how simple it is

--- 2018/day14/chocolate_charts.py
+++ 2018/day14/chocolate_charts.py
@@ -31,5 +31,48 @@ def scoresFrom(scoreStartIndex, outlook=10):
 
     return rscores[scoreStartIndex:scoreStartIndex+outlook]
 
+def findPatternInScores(rscores, pattern, ci):
+    if len(rscores) < len(pattern):
+        return -1
+
+    for i in range(ci, len(rscores)):
+        cp = ''.join(map(str, rscores[i-len(pattern): i]))
+        if cp == pattern:
+            return i - 1
+
+    return -1
+
+def findPattern(pattern):
+    rscores = [3, 7]
+    e1 = 0
+    e2 = 1
+    checkIndex = 0
+
+    patternIndex = findPatternInScores(rscores, pattern, checkIndex)
+    while patternIndex == -1:
+        checkIndex = len(rscores) - 1
+        combined = rscores[e1] + rscores[e2]
+        rscores.extend(makeRcps(combined))
+        sl = len(rscores)
+        te1 = (rscores[e1] + 1) % sl
+        te2 = (rscores[e2] + 1) % sl
+        if e1 + te1 > sl - 1:
+            e1 = (e1 + te1) - sl 
+        else:
+            e1 = (e1 + te1)
+    
+        if (e2 + te2) > sl - 1:
+            e2 = (e2 + te2) - sl
+        else:
+            e2 = (e2 + te2)
+    
+        patternIndex = findPatternInScores(rscores, pattern, checkIndex)
+
+
+    return patternIndex - len(pattern) + 1
+
 for ssi in [9, 5, 18, 2018, 209231]:
     print(''.join(map(str,scoresFrom(ssi))))
+
+for p in ['51589', '01245', '92510', '59414', '209231']:
+    print('Found pattern ' + p + ' after ' + str(findPattern(p)) + ' recipies')
